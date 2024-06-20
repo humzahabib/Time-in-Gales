e8456ce7fde280e35690a44aa2fb334ba1f90ebf
Hamza Habib <muhammadhamzahabib@outlook.com>
2024-06-21 03:03:04 +0500

    Player Controller Made
    - Player Movement
    - Player rotation towards camera

--- Time in Gales/Assets/Scripts/Player.cs
+++ Time in Gales/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -24,15 +25,37 @@ public class Player : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput);
+        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput) * speed;
 
-        if (direction.magnitude >= 0.1f)
+        controller.Move(direction * Time.deltaTime);
+
+
+
+        
+        // Aiming logic
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
         {
-            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg * cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Vector3 moreDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moreDir.normalized * speed * Time.deltaTime);
+            Vector3 pointToLook = ray.GetPoint(rayDistance);
+            Vector3 lookDir = pointToLook - transform.position;
+            lookDir.y = 0;
+            // Rotate the player to face the mouse cursor
+            transform.forward = lookDir;
         }
+
+
+        //if (direction.sqrMagnitude >= 0.1f)
+        //{
+        //    targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg * cam.eulerAngles.y;
+        //    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        //    transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        //    Vector3 moreDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+        //    controller.Move(moreDir.normalized * speed * Time.deltaTime);
+        //}
+
+
     }
 }
