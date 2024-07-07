e6f52c4e3d3a91082850245fc403b0a74b885409
Hamza Habib <muhammadhamzahabib@outlook.com>
2024-07-07 16:42:13 +0500

    AudioManager

--- Time in Gales/Assets/Scripts/Utils/AudioManager.cs
+++ Time in Gales/Assets/Scripts/Utils/AudioManager.cs
@@ -5,20 +5,6 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioSource audioSource;
-    static AudioManager instance;
-
-
-public static AudioManager Instance
-        { get { return instance; } }
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (instance == null)
-            instance = this;
-        else
-            Destroy(this.gameObject);
-        DontDestroyOnLoad(gameObject);
-    }
 
     public void Play(AudioClip clip)
     {
